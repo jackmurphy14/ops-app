39eb1276d1538a5356e1ca15b20d4cf9da19cfd1
Jack Murphy <jmurphy@sensoteq.co.uk>
2023-03-28 12:03:50 +0100

    hello world test 2

--- hello.py
+++ hello.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World'
+    return 'Index Page'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+
 
 if __name__ == '__main__':
     app.run()
