2f64e00149953310782b61c586ed3f483bad9e0d
Jack Murphy <jmurphy@sensoteq.co.uk>
2023-03-28 16:46:13 +0100

    Deployment test

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 #routes file
-from flask import render_template
-from app import app
+from flask import render_template, Flask
 from app.forms import LoginForm
 
+app = Flask(__name__)
 @app.route('/')
 @app.route('/login')
 def login():
