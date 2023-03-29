02df20a6cc451b18aa0316648a0094e315d3a8c3
Jack Murphy <jmurphy@sensoteq.co.uk>
2023-03-29 16:45:39 +0100

    registration first commit

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo, ValidationError
+from app.models import User
 
 #defines user login form
 class LoginForm(FlaskForm):
@@ -9,3 +10,16 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
+#defines registration form
+class RegistrationForm(FlaskForm):
+    first_name = StringField('Enter your forename: ', validators=[DataRequired()])
+    last_name = StringField('Enter your surname: ', validators=[DataRequired()])
+    username = StringField('Create a username: ', validators=[DataRequired()])
+    password = PasswordField('Create a password: ', validators=[DataRequired])
+    password2 = PasswordField('Repeat your password: ', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Create')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username already taken')
