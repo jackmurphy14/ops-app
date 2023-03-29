3df3685890209911d28563cc97a447aa17d17796
Jack Murphy <jmurphy@sensoteq.co.uk>
2023-03-29 09:38:12 +0100

    login route change

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,5 @@ from app.forms import LoginForm
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}'.format(form.username.data))
     return render_template('login.html', form=form)
 
